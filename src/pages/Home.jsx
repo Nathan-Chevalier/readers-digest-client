b6fbf15e7aadde4dc8196caffded423561263efb
Nathan Chevalier <cerberusgfx@gmail.com>
2023-11-07 14:26:04 -0600

    Initial testing state for a book array of objects implemented

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,23 @@
+import { useState } from "react";
+
 export const Home = () => {
-  return <>HOME PAGE YOU GOT THERE!</>;
+    const [allBooks, setAllBooks] = useState([
+        {
+            id: 1,
+            title: "useState Not Working",
+            author: "Nathan Chevalier",
+            isbn_number: 12345678,
+            cover_image: "https://upload.wikimedia.org/wikipedia/commons/6/67/Nope_logo.jpg?20220811105328",
+            user_id: 1,
+            is_owner: true,
+            categories: [
+                {
+                    "id": 1,
+                    "name": "Not Real Data"
+                }
+            ]
+        }
+    ])
+
+  return <>HOME PAGE, YOU GOT THERE!</>;
 };
