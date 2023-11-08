02b8975d657f1a0c2d67ba9082154b658e184476
Nathan Chevalier <cerberusgfx@gmail.com>
2023-11-08 09:51:52 -0600

    Routing to create fixed, category state working in create

--- src/pages/AddBook.jsx
+++ src/pages/AddBook.jsx
@@ -1,3 +1,19 @@
+import { useEffect, useState } from "react";
+import { getAllCategories } from "../utils/fetch";
+
 export const AddBook = () => {
+    const [allCategories, setAllCategories] = useState([
+        {id: 1, name: "Fetch No Work"},
+        {id: 2, name: "Fix your fetch"}
+    ])
+
+    useEffect(() => {
+        getAllCategories().then((categoryArray) => {
+            setAllCategories(categoryArray)
+        })
+    }, [])
+
+
+
   return <>ADD A BOOK</>;
 };
