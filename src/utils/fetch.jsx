85b595f1e33b19b83068187276b534bbf9515db6
Nathan Chevalier <cerberusgfx@gmail.com>
2023-11-08 09:30:39 -0600

    Categories fetch built to serve create book form

--- src/utils/fetch.jsx
+++ src/utils/fetch.jsx
@@ -7,3 +7,13 @@ export const getAllBooks = () => {
     },
   }).then((res) => res.json());
 };
+
+export const getAllCategories = () => {
+  return fetch(`http://localhost:8000/categories`, {
+    headers: {
+      Authorization: `Token ${
+        JSON.parse(localStorage.getItem("book_token")).token
+      }`,
+    },
+  }).then((res) => res.json());
+};
