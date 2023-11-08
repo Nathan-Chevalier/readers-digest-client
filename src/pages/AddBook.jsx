05e79d736ae205f5a027a4afa3b0a853aef6e730
Nathan Chevalier <cerberusgfx@gmail.com>
2023-11-08 10:11:19 -0600

    POST created for adding a book, routing back to /

--- src/pages/AddBook.jsx
+++ src/pages/AddBook.jsx
@@ -1,11 +1,15 @@
 import { useEffect, useState } from "react";
 import { getAllCategories } from "../utils/fetch";
+import { useNavigate } from "react-router-dom";
 
 export const AddBook = () => {
+const navigate = useNavigate()
+
     const [allCategories, setAllCategories] = useState([
         {id: 1, name: "Fetch No Work"},
         {id: 2, name: "Fix your fetch"}
     ])
+    const [newBook, setNewBook] = useState({})
 
     useEffect(() => {
         getAllCategories().then((categoryArray) => {
@@ -13,6 +17,22 @@ export const AddBook = () => {
         })
     }, [])
 
+    const addBook = async (evt) => {
+        evt.preventDefault();
+
+        await fetch("http://localhost:8000/books", {
+            method: "POST",
+            headers: {
+                Authorization: `Token ${
+                    JSON.parse(localStorage.getItem("rock_token")).token
+                }`,
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newBook)
+        })
+
+        navigate("/")
+    }
 
 
   return <>ADD A BOOK</>;
